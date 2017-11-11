9c0cd55d41582187b0a56363ae80f81bb52c3dae
asqdg7724 <asqdg@naver.com>
2017-11-11 12:15:57 +0900

    스톱워치와 기능 추가 타이머는 아직 기능 추가 X

--- app/src/main/java/com/example/home_777/alarm_application/MainActivity.java
+++ app/src/main/java/com/example/home_777/alarm_application/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.home_777.alarm_application;
 
+import android.app.TabActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,16 +8,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     Button btnAlarmAdd = (Button) findViewById(R.id.btnAlarmAdd);
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("아두이노 연동 어플");
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabAlarmSpec = tabHost.newTabSpec("ALARM").setIndicator("알람");
+        tabAlarmSpec.setContent(R.id.tabAlarm);
+        tabHost.addTab(tabAlarmSpec);
+
+        intent = new Intent(this, stopwatch.class);
+        TabHost.TabSpec tabStopSpec = tabHost.newTabSpec("STOPWATCH").setIndicator("스톱워치");
+        tabStopSpec.setContent(R.id.tabStop);
+        tabHost.addTab(tabStopSpec);
+
+        TabHost.TabSpec tabTimerSpec = tabHost.newTabSpec("TIMER").setIndicator("타이머");
+        tabTimerSpec.setContent(R.id.tabTimer);
+        tabHost.addTab(tabTimerSpec);
+
+        tabHost.setCurrentTab(0);
+
         btnAlarmAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ app/src/main/java/com/example/home_777/alarm_application/timer.java
@@ -0,0 +1,10 @@
+package com.example.home_777.alarm_application;
+
+import android.app.Activity;
+
+/**
+ * Created by home-777 on 2017-11-11.
+ */
+
+public class timer extends Activity {
+}
